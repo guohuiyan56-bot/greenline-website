5aedbd865b5954d72cbd3301657bdd012b4c2eca
李燕 <info@greenlineint.com>
2026-08-17 16:05:54 +0800

    Add catalogs: christmas, louver, ys-series, geyas-sanitary

---
+++ catalog/louver/data.js
@@ -0,0 +1,32 @@
+window.FLIP = {
+ "total": 12,
+ "images": [
+  "pages/full/p000.jpg",
+  "pages/full/p001.jpg",
+  "pages/full/p002.jpg",
+  "pages/full/p003.jpg",
+  "pages/full/p004.jpg",
+  "pages/full/p005.jpg",
+  "pages/full/p006.jpg",
+  "pages/full/p007.jpg",
+  "pages/full/p008.jpg",
+  "pages/full/p009.jpg",
+  "pages/full/p010.jpg",
+  "pages/full/p011.jpg"
+ ],
+ "thumbs": [
+  "pages/thumbs/t000.jpg",
+  "pages/thumbs/t001.jpg",
+  "pages/thumbs/t002.jpg",
+  "pages/thumbs/t003.jpg",
+  "pages/thumbs/t004.jpg",
+  "pages/thumbs/t005.jpg",
+  "pages/thumbs/t006.jpg",
+  "pages/thumbs/t007.jpg",
+  "pages/thumbs/t008.jpg",
+  "pages/thumbs/t009.jpg",
+  "pages/thumbs/t010.jpg",
+  "pages/thumbs/t011.jpg"
+ ],
+ "toc": []
+};
